cc9a094a353d1785525bbb8fac34a9aa2b75587e
Izandro <izandromm@gmail.com>
2023-12-17 12:00:57 -0300

    copy project to migrate structure to spring data rest

---
+++ crud_spring_jpa_with_spring_data_rest/src/main/java/com/testproject/cruddemo/entity/Employee.java
@@ -0,0 +1,80 @@
+package com.testproject.cruddemo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="employee")
+public class Employee {
+
+    // define fields
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="first_name")
+    private String fisrtName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="email")
+    private String email;
+
+    // define constructors
+    public Employee(){
+
+    }
+
+    public Employee(String fisrtName, String lastName, String email) {
+        this.fisrtName = fisrtName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    // define getter/setter
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFisrtName() {
+        return fisrtName;
+    }
+
+    public void setFisrtName(String fisrtName) {
+        this.fisrtName = fisrtName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // define toString
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + fisrtName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+}
